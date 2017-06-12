f4e69acf29a9afb3f3d894243accfa343b1f71a5
TobiasRoth <t.roth@unibas.ch>
2017-06-12 19:39:21 +0200

    New instance of package

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Two surveys of Swiss plant communities from Swiss Biodiversity Monitoring.
+#'
+#' The Swiss biodiversity monitoring (BDM) has among others monitored the presence/absence 
+#' of plant species at 1x1km squares. The data-set contains the data from 363 squares with exactly
+#' two surveys conducted by professional botanists between 2010 and 2014. 
+#' 
+#'
+#' @format A list with xx elements:
+#' \describe{
+#'   \item{y}{Array with presence/absence observed in 362 squares for 1733 species 
+#'   during two surveys.}
+#'   \item{elevation}{Vecotor with elevation (m) of each square.}
+#'   
+#'   
+#' }
+#' @source \url{http://www.biodiversitymonitoring.ch/}
+"plantsBDM"

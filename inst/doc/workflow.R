54a569bd52072901bf79c0bf51bfc5966137a6f1
TobiasRoth <t.roth@unibas.ch>
2017-06-15 14:56:04 +0200

    Build vignettes

--- inst/doc/workflow.R
+++ inst/doc/workflow.R
@@ -1,73 +1,95 @@
-## ---- echo=TRUE, message=FALSE, warning=FALSE, cache=FALSE---------------
+## ---- echo=TRUE, message=FALSE, warning=FALSE, cache=TRUE----------------
 library(devtools)
-install_github("TobiasRoth/detecfilter")
-library(detecfilter)
+install_github("TobiasRoth/detectionfilter")
+library(detectionfilter)
+
+## ---- cache=TRUE---------------------------------------------------------
+set.seed(1234)
+dat <- simcom(mu.FTfilter.lpsi = -0.5, mean.psi = 0.8, 
+              mu.FTfilter.lp = 1, mean.p = 0.8,
+              nsite = 200, nspec = 100, nrep = 2)
+
+## ---- cache=TRUE---------------------------------------------------------
+# Calculate observed meta-community (i.e. merge the observations from the two visits)
+commat_obs <- apply(dat$y, c(1,2), max)
+
+# Show species with few observations
+sort(apply(commat_obs, 2, sum))[1:10]
 
 ## ---- message=FALSE, warning=FALSE, cache=TRUE---------------------------
+# Package to fit hierarchical models of occurrence and abundance data
 library(unmarked)
+
+# Prepare detection-corrected meta-community matrix
 z <- array(NA, dim = c(dim(dat$y)[1], dim(dat$y)[2]))
-a0 <- numeric(dim(dat$y)[2])
-a0[!is.na(a0)] <- NA
-b1 <- numeric(dim(dat$y)[2])
-b1[!is.na(b1)] <- NA
+
+# Apply hierarchical model to all species seperate
 for(k in 1:dim(dat$y)[2]) {
-  d <- unmarkedFrameOccu(y = dat$y[,k,], siteCovs = data.frame(gradient = dat$gradient))
-  try({
-    res <- occu(~ 1 ~ gradient, data = d, se = TRUE)
-    a0[k] <- plogis(coef(res)["p(Int)"])
-    b1[k] <- coef(res)["psi(gradient)"]
-    z[,k] <- bup(ranef(res), stat = "mode")
- })
+  d <- unmarkedFrameOccu(y = dat$y[,k,], obsCovs = list(date = dat$date),
+                         siteCovs = data.frame(gradient = dat$gradient))
+  res <- occu(~ date ~ gradient, data = d, se = TRUE)
+  z[,k] <- bup(ranef(res), stat = "mode")
 }
 
-## ------------------------------------------------------------------------
-# Calculate community mean value of trait expression (CM)
-commat_obs <- apply(dat$y, c(1,2), max)
-CM_obs <- apply(commat_obs, 1, function(x) mean(dat$traitmat[dat$detected.at.all][x==1]))
-CM_cor <- apply(z>0.5, 1, function(x) mean(dat$traitmat[dat$detected.at.all][x], na.rm = TRUE))
-CM_true <- apply(dat$z_true, 1, function(x) mean(dat$traitmat[x==1]))
+## ---- cache=TRUE, fig.height=4-------------------------------------------
+# Function to calculate mean trait expression of species in a community (CM)
+CM <- function(x) mean(dat$traitmat[x])
+
+# Calculate CMs from observed meta-community matrix, from detection-corrected 
+# meta-community matrix and from true mata-community matrix
+CM_obs <- apply(commat_obs==1, 1, CM)
+CM_cor <- apply(z>0.5, 1, CM)
+CM_true <- apply(dat$z_true==1, 1, CM)
 
 # Plot CM along gradient
-plot(dat$gradient, CM_obs, pch = 16, cex = 0.7, ylim = c(-1.5, 1.5))
+plot(dat$gradient, CM_obs, pch = 16, cex = 0.7, ylim = c(-1, 1), 
+     xlab = "Gradient", ylab = "Community mean")
 points(dat$gradient, CM_true, cex = 0.7, col = "orange")
 points(dat$gradient, CM_cor, pch = "+", cex = 0.7, col = "red")
 abline(lm(CM_obs ~ dat$gradient))
 abline(lm(CM_true ~ dat$gradient), col = "orange")
 abline(lm(CM_cor ~ dat$gradient), col = "red")
-abline(h=mean(dat$traitmat), lty = 2)
-abline(h=0)
 
-## ---- eval=FALSE, include=FALSE------------------------------------------
-#  plot(dat$P[dat$detected.at.all], a0)
-#  abline(0,1)
-#  plot(dat$traitmat[dat$detected.at.all], a0)
-#  
-#  plot(dat$traitmat[dat$detected.at.all], b1, ylim = c(-2,2))
-#  abline(h=0)
-#  
-#  plot(dat$gradient, apply(commat_obs, 1, sum), pch = 16, cex = 0.7, ylim = c(0, 40))
-#  points(dat$gradient, apply(dat$z_true, 1, sum), cex = 0.7, col = "orange")
-#  points(dat$gradient, apply(z>0.5, 1, sum), pch = "+", cex = 0.7, col = "red")
-#  abline(h=0)
-#  abline(h=mean(dat$traitmat), lty = 2)
-#  
-#  # Calculate community mean value of trait expression (CM)
-#  commat_obs <- apply(dat$y, c(1,2), max)
-#  ausw <- b1 < 10
-#  CM_obs <- apply(commat_obs[,ausw], 1, function(x) mean(dat$traitmat[x==1]))
-#  CM_cor <- apply(z[,ausw], 1, function(x) mean(dat$traitmat[x==1], na.rm = TRUE))
-#  CM_true <- apply(dat$z_true, 1, function(x) mean(dat$traitmat[x==1]))
-#  
-#  # Plot CM along gradient
-#  plot(dat$gradient, CM_obs, pch = 16, cex = 0.7, ylim = c(-1.5, 1.5))
-#  points(dat$gradient, CM_true, cex = 0.7, col = "orange")
-#  points(dat$gradient, CM_cor, pch = "+", cex = 0.7, col = "red")
-#  abline(h=0)
-#  abline(h=mean(dat$traitmat), lty = 2)
-#  
-#  
-#  ausw <- commat_obs==0 & z == 1
-#  plot(dat$gradient, apply(ausw, 1, sum))
-#  plot(dat$gradient, apply(ausw, 1, function(x) ))
-#  
+## ---- cache=TRUE---------------------------------------------------------
+set.seed(1234)
+dat <- simcom(mu.FTfilter.lpsi = -0.5, mean.psi = 0.5, 
+              mu.FTfilter.lp = 2, mean.p = 0.5,
+              nsite = 200, nspec = 100, nrep = 2)
+
+# Observed community matrix
+commat_obs <- apply(dat$y, c(1,2), max)
+
+# Number of observed species
+ncol(commat_obs)
+
+## ---- cache=TRUE---------------------------------------------------------
+sum(apply(dat$z_true, 2, sum)>0)
+
+## ------------------------------------------------------------------------
+# Proportion of  species observed at least once but in less than 10% of sites
+mean(apply(commat_obs, 2, sum) < 20)
+
+## ---- message=FALSE, warning=FALSE, cache=TRUE---------------------------
+# Estimate detection-corrected meta-community matrix
+z <- array(NA, dim = c(dim(dat$y)[1], dim(dat$y)[2]))
+for(k in 1:dim(dat$y)[2]) {
+  d <- unmarkedFrameOccu(y = dat$y[,k,], obsCovs = list(date = dat$date),
+                         siteCovs = data.frame(gradient = dat$gradient))
+  res <- occu(~ date ~ gradient, data = d, se = TRUE)
+  z[,k] <- bup(ranef(res), stat = "mode")
+}
+
+# Calculate CMs
+CM_obs <- apply(commat_obs==1, 1, CM)
+CM_cor <- apply(z>0.5, 1, CM)
+CM_true <- apply(dat$z_true==1, 1, CM)
+
+# Plot CMs along gradient
+plot(dat$gradient, CM_obs, pch = 16, cex = 0.7, ylim = c(-1, 1), 
+     xlab = "Gradient", ylab = "Community mean")
+points(dat$gradient, CM_true, cex = 0.7, col = "orange")
+points(dat$gradient, CM_cor, pch = "+", cex = 0.7, col = "red")
+abline(lm(CM_obs ~ dat$gradient))
+abline(lm(CM_true ~ dat$gradient), col = "orange")
+abline(lm(CM_cor ~ dat$gradient), col = "red")
 
